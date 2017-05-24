f7588198083b79f033ab0e7a90e726959ae89037
devixly <devixly@gmail.com>
2017-05-24 12:25:50 +0300

    Add basic svgMap

--- Travel/App_Start/BundleConfig.cs
+++ Travel/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace Travel
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquerymap").Include(
+                        "~/Scripts/jquery.svgworldmap-2.1*"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,7 +28,8 @@ namespace Travel
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/svgmap.css"));
         }
     }
 }
